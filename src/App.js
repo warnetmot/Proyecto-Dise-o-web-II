8f1144b8a1a17166b197f8526c735df52c3ab03d
warnetmot <reyfernandojuarez@gmail.com>
2024-10-29 17:10:55 -0400

    subiendo actualisacion

--- src/App.js
+++ src/App.js
@@ -34,6 +34,7 @@ function App() {
             <Gallery />
             <Pagination />
             <Footer />
+            
         </div>
     );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,16 +12,16 @@ function Footer() {
             <div className="container-fluid tm-container-small">
                 <div className="row">
                     <div className="col-lg-6 col-md-12 col-12 px-5 mb-5">
-                        <h3 className="tm-text-primary mb-4 tm-footer-title">About Catalog-Z</h3>
-                        <p>Catalog-Z is a free Bootstrap 5 HTML Template for video and photo websites. You can use this TemplateMo layout for front-end integration.</p>
+                        <h3 className="tm-text-primary mb-4 tm-footer-title">Acerca de WallGallery</h3>
+                        <p>WallGallery es una página donde puedes encontrar fondos de pantalla de alta calidad para personalizar tus dispositivos. Navega y descarga tus fondos favoritos gratuitamente.</p>
                     </div>
                     <div className="col-lg-3 col-md-6 col-sm-6 col-12 px-5 mb-5">
-                        <h3 className="tm-text-primary mb-4 tm-footer-title">Our Links</h3>
+                        <h3 className="tm-text-primary mb-4 tm-footer-title">Enlaces Útiles</h3>
                         <ul className="tm-footer-links pl-0">
-                            <li><a href="#">Advertise</a></li>
-                            <li><a href="#">Support</a></li>
-                            <li><a href="#">Our Company</a></li>
-                            <li><a href="#">Contact</a></li>
+                            <li><a href="#">Publicidad</a></li>
+                            <li><a href="#">Ayuda</a></li>
+                            <li><a href="#">Sobre Nosotros</a></li>
+                            <li><a href="#">Contacto</a></li>
                         </ul>
                     </div>
                     <div className="col-lg-3 col-md-6 col-sm-6 col-12 px-5 mb-5">
@@ -47,8 +47,8 @@ function Footer() {
                                 </a>
                             </li>
                         </ul>
-                        <a href="#" className="tm-text-gray text-right d-block mb-2">Terms of Use</a>
-                        <a href="#" className="tm-text-gray text-right d-block">Privacy Policy</a>
+                        <a href="#" className="tm-text-gray text-right d-block mb-2">Términos de Uso</a>
+                        <a href="#" className="tm-text-gray text-right d-block">Política de Privacidad</a>
                     </div>
                 </div>
             </div>
@@ -57,4 +57,3 @@ function Footer() {
 }
 
 export default Footer;
-
